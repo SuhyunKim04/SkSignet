787d329bb875a77ee2086c8b3341ab7ac500ddf1
Suhyun Kim <kim00431@algonquinlive.com>
2023-09-01 21:44:55 -0400

    updates

--- js/common.js
+++ js/common.js
@@ -1,5 +1,8 @@
 const modal = () => {
     const modalWrap = document.querySelector('.modalWrap');
+    if(!modalWrap) {
+        return false;
+    }
     const close = document.querySelector('.close');
     const openvideo = document.querySelector('.openVideo');
 
@@ -19,14 +22,16 @@ console.log(close)
 }
 
 const drop = () => {
-    const wrap = document.querySelector('.visualContents');
     const arrowBtn = document.querySelector('.choose .btn');
+    if(!arrowBtn) {
+        return false;
+    }
     const dropDown = document.querySelector('.chooseDrop');
     const icon = document.querySelector('.choose .icon');
     let active = 0;
 
     arrowBtn.addEventListener('click',(e) => {
-
+        
         dropDown.classList.toggle('open')
         icon.classList.toggle('open')
     })
@@ -62,35 +67,35 @@ const test = () =>{
     });
 
 }
-const test1 = () =>{
-    const Btns = document.querySelectorAll('.chooseDrop-a a');
-    const msg = document.querySelector('.select span');
-    const link = document.querySelector('.link-go');
-    const go = document.querySelector('.btn-go');
-    let url ='';
-    console.log(url);
-    Btns.forEach((e,i) => {
-        console.log(e)
-        e.addEventListener('click',(event)=>{
-            event.preventDefault();
-            const target = event.target
-            msg.innerHTML = target.innerHTML
-            url = target.getAttribute('href')
-            link.setAttribute('href',url)
-            console.log(target.getAttribute('href'))
-        })
+// const test1 = () =>{
+//     const Btns = document.querySelectorAll('.chooseDrop-a a');
+//     const msg = document.querySelector('.select span');
+//     const link = document.querySelector('.link-go');
+//     const go = document.querySelector('.btn-go');
+//     let url ='';
+//     console.log(url);
+//     Btns.forEach((e,i) => {
+//         console.log(e)
+//         e.addEventListener('click',(event)=>{
+//             event.preventDefault();
+//             const target = event.target
+//             msg.innerHTML = target.innerHTML
+//             url = target.getAttribute('href')
+//             link.setAttribute('href',url)
+//             console.log(target.getAttribute('href'))
+//         })
 
-    })
-    go.addEventListener('click',(e)=>{
-        console.log('hello')
-        console.log(url);
-        if(url === ''){
-            alert('아래에서 항목 선택하세요')
-            return false;
-        }
-        window.location.href = url;
-    })
-}
+//     })
+//     go.addEventListener('click',(e)=>{
+//         console.log('hello')
+//         console.log(url);
+//         if(url === ''){
+//             alert('아래에서 항목 선택하세요')
+//             return false;
+//         }
+//         window.location.href = url;
+//     })
+// }
 
 const modal_menu = () => {
     const depth_list = document.querySelectorAll('.depth_list');
@@ -98,7 +103,7 @@ const modal_menu = () => {
     const item = document.querySelector('.pageHeaderItems');
     
 
-    const hideMenu = (list) => {
+    const hideMenu = () => {
         modal_box.forEach((list) => list.classList.remove('on'));
     }
     depth_list.forEach((list,i) => {
@@ -109,7 +114,6 @@ const modal_menu = () => {
             console.log('text')
         })
     });
-
 
     item.addEventListener('mouseleave', () => {
         hideMenu();
@@ -122,11 +126,99 @@ const modal_menu = () => {
     // })
 }
 
+const cards = () =>{
+    const items = document.querySelectorAll('.peopleCards .items')
+    let now = 0;
+    items[now].classList.add('on');
+    
+    items.forEach( (item,index) => {
+        
+        item.addEventListener('click',(event)=>{ 
+            console.time('check')
+            const target = event.currentTarget; 
+            const status = target.classList.contains('on'); 
+            if(status){
+                target.classList.remove('on')
+            }else{
+                items.forEach( e =>{
+                    e.classList.remove('on')
+                })
+                target.classList.add('on');
+                now = index;
+            }
+ 
+            console.timeEnd('check')
+            
+        })
 
+    })
+
+}
+
+const cards2 = () => {
+    const items = document.querySelectorAll('.peopleCards .items')
+ 
+    let now = 0;
+   
+
+    const reset = () => {
+        items[now].classList.remove('on');
+        items[now].querySelector('dd').style.height = '0px'; 
+    }
+
+    const update = () => { 
+        items[now].classList.add('on');
+        items[now].querySelector('dd').style.height = 
+        items[now].querySelector('p').offsetHeight + 'px';
+    }
+    
+
+    update();
+
+    items.forEach( (item,index) => { 
+        item.addEventListener('click',(event)=>{ 
+            // console.time('check')
+            const target = event.currentTarget;    
+            const child = target.querySelector('dd');
+            const size = child.querySelector('p').offsetHeight;
+            // const li = (now === index); 
+
+     
+           if(now !== index) {  
+                reset(); 
+                target.classList.add('on');   
+                child.style.height = `${size}px`;
+                now = index;  
+            }   else {
+                reset(); 
+             
+            }
+            // console.timeEnd('check')
+        })
+        
+    })
+    
+}
+
+
+// if(li){
+//     target.classList.remove('on') 
+//     child.style.height = `0px`; 
+// }else{ 
+  
+
+//     target.classList.add('on');  
+//     console.log('else') 
+//     child.style.height = `${size}px`;
+//     now = index; 
+// }  
 
 slideBanners();
 modal();
 drop();
-test();
-test1();
+// test();
 modal_menu();
+
+cards2();
+// cards();
+ 
